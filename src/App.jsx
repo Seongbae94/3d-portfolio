304f0d9efe47f6566d91b4e9b301daf34813df41
Seongbae94 <seongbaelee944@gmail.com>
2023-11-20 12:01:29 -0500

    project setup
    1. tailwind setup
    2. src file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        {" "}
+        hello world
+      </h1>
+    </div>
+  );
 };
 
 export default App;
